2522149dc06ce2ed6f298f03aa41096e09abaf29
ncortim <ncortim@proton.me>
2025-02-12 18:10:09 +0100

    initial commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,12 @@
+!#/user/bin/env groovy
+
+def call() {
+    echo 'building the docker image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASSWD', usernameVariable: 'USER')]) {
+    sh 'docker image build -t ncortim/demo-app:jma-1.3 .'
+    sh 'echo $PASSWD | docker login -u $USER --password-stdin'
+    sh 'docker push ncortim/demo-app:jma-1.3'
+    }
+
+}
+
